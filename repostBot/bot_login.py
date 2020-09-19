798e8a4fd1c320cb0a22bf5ca4874b34b3ac8a8d
Siddhesh Dongare <70208548+SiddheshDongare@users.noreply.github.com>
2020-09-19 22:00:31 +0530

    Update bot_login.py

--- repostBot/bot_login.py
+++ repostBot/bot_login.py
@@ -3,12 +3,12 @@ import os
 
 def bot_login():
     try:
-        print("logging in: ", os.getenv("Sidikulous"))
-        reddit = praw.Reddit(client_id = os.getenv("dpNlSeAlbrNlGA"),
-                    client_secret = os.getenv("I9HwugR1YLiltOCwQI0yxtzJWMQ"),
+        print("logging in: ", os.getenv(Sidikulous))
+        reddit = praw.Reddit(client_id = os.getenv(dpNlSeAlbrNlGA),
+                    client_secret = os.getenv(I9HwugR1YLiltOCwQI0yxtzJWMQ),
                     user_agent = 'PRAW API tutorial Python Script',
-                    username = os.getenv("Sidikulous"),
-                    password = os.getenv("siddhesh12"))   
+                    username = os.getenv(Sidikulous),
+                    password = os.getenv(siddhesh12))   
         print("logged in!")
     except:
         print("Login failed")
